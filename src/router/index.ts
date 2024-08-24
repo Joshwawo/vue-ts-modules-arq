c9efb8d67709fd1276abff5a06ef55030da5b6c3
Joshwawo <masacota_masacota@hotmail.com>
2024-08-23 21:51:00 -0700

    chore: Remove unused code and clean files

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,11 +7,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -20,7 +15,7 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/inicio',
+      path: '/',
       name: 'inicio',
       component: () => import('@/modules/home/layouts/HomeLayout.vue'),
       children: HomeRouter
